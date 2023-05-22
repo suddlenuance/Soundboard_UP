a8453da4d73eab9032f05d79d8fa6447219a2f0b
enrique <quique051605@gmail.com>
2023-05-22 13:59:45 -0700

    update

--- app/src/main/java/com/example/soundboard/MainActivity.java
+++ app/src/main/java/com/example/soundboard/MainActivity.java
@@ -2,13 +2,145 @@ package com.example.soundboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_1;
+
+    private Button btn_2;
+
+    private Button btn_3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_1 = findViewById(R.id.btn_1);
+        btn_2 = findViewById(R.id.btn_2);
+        btn_3 = findViewById(R.id.btn_3);
+
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playbtn_1();
+            }
+        });
+
+        btn_2.setOnClickListener(new View.OnClickListener() {
+             @Override
+            public void onClick(View v) {
+                playBtn_2();
+            }
+        });
+
+        btn_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playbtn_3();
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void playbtn_1() {
+            MediaPlayer mp = MediaPlayer.create(this, R.raw.sam)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
