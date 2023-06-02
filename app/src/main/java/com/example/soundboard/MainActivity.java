e7b2381e2c0a9295ebb0f1c00f651b4ca39d2583
200913119 <quique051605@gmail.com>
2023-06-02 13:50:57 -0700

    update

--- app/src/main/java/com/example/soundboard/MainActivity.java
+++ app/src/main/java/com/example/soundboard/MainActivity.java
@@ -9,11 +9,9 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn_1;
+    private Button btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9, btn_10, btn_11, btn_12;
 
-    private Button btn_2;
 
-    private Button btn_3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
         btn_1 = findViewById(R.id.btn_1);
         btn_2 = findViewById(R.id.btn_2);
         btn_3 = findViewById(R.id.btn_3);
+        btn_4 = findViewById(R.id.btn_4);
+        btn_5 = findViewById(R.id.btn_5);
+        btn_6 = findViewById(R.id.btn_6);
+        btn_7 = findViewById(R.id.btn_7);
+        btn_8 = findViewById(R.id.btn_8);
+        btn_9 = findViewById(R.id.btn_9);
+        btn_10 = findViewById(R.id.btn_10);
+        btn_11 = findViewById(R.id.btn_11);
+        btn_12 = findViewById(R.id.btn_12);
+
+
 
         btn_1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,16 +47,81 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btn_2.setOnClickListener(new View.OnClickListener() {
+        btn_3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 playbtn_3();
             }
         });
+        btn_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playBtn_4();
+            }
+        });
+        btn_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playBtn_5();
+            }
+        });
+        btn_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playBtn_6();
+            }
+        });
+        btn_7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playBtn_7();
+            }
+        });
+        btn_8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playBtn_8();
+            }
+        });
+        btn_9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playBtn_9();
+            }
+        });
+        btn_10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playBtn_10();
+            }
+        });
+        btn_11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playBtn_11();
+            }
+        });
+        btn_12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playBtn_12();
+            }
+        });
 
 
+        private void playbtn_1() {
+            MediaPlayer mp = MediaPlayer.create(this, R.raw.sample_meow);
+            mp.start();
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    mp.stop();
+                    mp.release();
+                    mp = null;
+                }
+            });
 
-
+        }
 
 
 
